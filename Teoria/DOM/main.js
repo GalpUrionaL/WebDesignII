bf7dfe828cb08b12ddaa9008c1d3b0c886616ea4
GalpUrionaL <gpul.950511@gmail.com>
2022-04-14 09:41:53 -0400

    update

--- Teoria/DOM/main.js
+++ Teoria/DOM/main.js
@@ -1,1 +1,16 @@
 //alert('Hola!');
+/**DOM Manipular el documento HTML
+ * a travez de JS
+ */
+let cajas = document.getElementsByClassName('caja');
+//cajas[0].innerHTML = '<h1> Caja </h1>'; // incrusta codigo HTML
+//cajas[2].textContent = 'Tercera Caja'; // solo texto
+/**Si realizo ls cambios en consola en navegador los cambios
+ * son temporales, mientras que en code no es así.
+ */
+
+let divs = document.getElementsByTagName('div');
+
+let primera = document.getElementById('primera');
+
+primera.textContent = 'Primera Caja';
