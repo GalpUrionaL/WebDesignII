c558ab104ab7057f5fa2682b47a84ba9f8415c7a
GalpUrionaL <gpul.950511@gmail.com>
2022-04-21 10:31:01 -0400

    update

---
+++ Teoria/JQuery/efectos.js
@@ -0,0 +1,34 @@
+// Funcion autoinvocada
+//$(function() { // una vez cargue todo el documento recien se va ejecutar la funcion
+// alert('Funciona');
+//alert('Funciona jquery');
+function Efecto() {
+    // Efectos
+    /** $(selector).efecto(velocidad, callback)*/
+    // Hide oculta
+    /*$('.caja').hide(3000, function() {
+        alert('oculto');
+    });*/
+
+    //Show 
+    //$('.caja').show(2000);
+
+    //Toggle
+    //$('.caja').toggle(1000); //alterna las dos anterioes
+
+    //FadeIn fadeOut se desvanece
+    //$('.caja').fadeIn(2000); // se muestra de a poco
+    //$('.caja').fadeOut(3000);
+    //$('.caja').fadeToggle(3000);
+
+    //Slide
+    /* $('.caja').hide();
+     $('.caja').slideDown(3000);*/
+
+    /*$('.caja').show();
+    $('.caja').slideUp(3000);*/
+
+
+
+}
+//});
